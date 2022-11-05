4cd2eeb0a08950286069e9368401e1280a347269
Lawrence Liu <lawrenceliu1212@gmail.com>
2022-11-05 18:20:54 -0400

    Logout button works

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -104,7 +104,7 @@ export default function AppBanner() {
                         <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>⌂</Link>
                     </Typography>
                     <Box sx={{ flexGrow: 1 }}>{editToolbar}</Box>
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <Box sx={{ display: { xs: 'none', md: 'flex', zIndex: '1' } }}>
                         <IconButton
                             size="large"
                             edge="end"
